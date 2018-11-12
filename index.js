9095b7e8bbc78fc4a81fbafca10e6bddd9a4a873
James Lotruglio <jimlotruglio@yahoo.com>
2018-11-12 19:35:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function BoardMember(name, homeState, training){
    this.training = training
 }
 
-   User.prototypethis.veto = function(){
+   BoardMember.prototype.veto = function(){
      return 'No, I must disagree'
    }
    
